361d3612509f780df9a19ad13dbdc22dcd397bbe
Adetu Daniel <adetud680@gmail.com>
2025-12-15 08:51:02 +0100

    Fix Task 1: Exception handling matches checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,24 +1,24 @@
-  GNU nano 8.6                     MySQLServer.py
 #!/usr/bin/python3
 
 import mysql.connector
-from mysql.connector import Error
 
-def create_database():
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as e:
+    print("Error while connecting to MySQL", e)
+
+finally:
     try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=""
-        )
-
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(
-                "CREATE DATABASE IF NOT EXISTS alx_book_store"
-            )
-            print("Database 'alx_book_store' created successfully!")
-
-                               [ Read 30 lines ]
-^G Help      ^O Write Out ^F Where Is  ^K Cut       ^T Execute   ^C Location
-^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
+        cursor.close()
+        connection.close()
+    except Exception:
+        pass
